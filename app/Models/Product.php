9e28f86166c6d9b5d9646b960c2aa672c31bcb1c
Xianhui Wang <lizzy@192.168.0.103>
2019-06-12 20:35:20 +0800

    added low stock out of stock and add to cart button if in stock

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,10 +10,34 @@ class Product extends Model
 {
 
       public function hasLowStock(){
+       
+
+         if ($this->outOfStock()){
+         	
+         	return false;
+         }
+        
+         return (bool) ($this->stock<=10);
 
 
+      }
 
-      	
+      public function outOfStock(){
+          
+          return $this->stock===0;
+
+      }
+
+      public function inStock(){
+
+          return $this->stock>=1;
+          
+      }
+
+      public function hasStock($quantity){
+
+      	return $this->stock>=$quantity;
+
       }
 
 }
